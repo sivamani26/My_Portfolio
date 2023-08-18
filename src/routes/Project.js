3d65b1c47072a1c02da4965489f25513871839ba
sivamani26 <venkat.sivamani.04@gmail.com>
2023-08-18 18:38:27 +0530

    Ready to diploy

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from  '../Components/Navbar';
 import HeroImage2 from '../Components/HeroImage2';
 import Footer from '../Components/Footer';
-import PricingCard from '../Components/PricingCard';
 import Work from '../Components/Work';
 
 const Project = () => {
@@ -11,10 +10,9 @@ const Project = () => {
         <Navbar />
         <HeroImage2 heading="PROJECTS." text="Some of my most recent works" />
         <Work />
-        <PricingCard />
         <Footer />
       </div>
   );
 };
 
-export default Project
+export default Project;
